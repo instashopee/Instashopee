9cffee99369c4f9e48352c43607d362dd0b81964
instashopee <instashopeeonline@gmail.com>
2023-09-07 19:33:26 +0530

    c

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -571,14 +571,14 @@ if(!error){
                 </span>}
                 <button disabled={product.availableQty <=0?true:false} onClick={() =>
                     buyNow(slug, 1, product.price, product.title, size, color)
-                  } className="flex ml-1 text-white bg-red-500 disabled:bg-red-300  border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-red-600 rounded">
+                  } className="flex ml-1 mb-5 text-white bg-red-500 disabled:bg-red-300  border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-red-600 rounded">
                   Buy Now
                 </button>
                 <button disabled={product.availableQty <=0?true:false} 
                   onClick={() => {
                     addToCart(slug, 1, product.price, product.title, size, color);
                   }}
-                  className="flex ml-5 text-white bg-red-500 disabled:bg-red-300  border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-red-600 rounded"
+                  className="flex ml-5 mb-5 text-white bg-red-500 disabled:bg-red-300  border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-red-600 rounded"
                 >
                   Add To Cart
                 </button>
