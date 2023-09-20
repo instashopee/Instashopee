57f53450f0a33c5a6f0644e6844c071e091d5870
Instashopee <instashopeeonline@gmail.com>
2023-09-20 19:56:01 +0530

    c

--- pages/login.js
+++ pages/login.js
@@ -123,7 +123,7 @@ const Login = () => {
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Sign in</button>
         </div>
-      <p >Or sign up with</p>
+      <p >Or sign up with </p>
               <button
                 type="button"
                 className="btn btn-link btn-floating-mx-1"
