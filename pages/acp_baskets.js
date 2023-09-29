22176af58ccd38dd1fef2a50a6e809d091b98623
Instashopee <instashopeeonline@gmail.com>
2023-09-29 18:53:54 +0530

    c

---
+++ pages/acp_baskets.js
@@ -0,0 +1,146 @@
+import Link from 'next/link'
+import React from 'react'
+import Product from "@/models/Product";
+import connectDb from "@/middleware/mongoose";
+import mongoose from 'mongoose';
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import Head from "next/head";
+import Size_Color from '@/components/Size_Color';
+
+const Hinges = ({products}) => {
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  return (
+    <div>
+      <Head>
+        <title>Products -  Instashopee</title>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+<div className="bg-white">
+      <div>
+        {/* Mobile filter dialog */}
+        <Transition.Root show={mobileFiltersOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-40 lg:hidden" onClose={setMobileFiltersOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-40 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+                  <div className="flex items-center justify-between px-4">
+                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                    <button
+                      type="button"
+                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                      onClick={() => setMobileFiltersOpen(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                    </div>
+
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-4">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900">New Arrivals</h1>
+
+          </div>
+
+          <section aria-labelledby="products-heading" className="pb-24 pt-6">
+            <h2 id="products-heading" className="sr-only">
+              Products
+            </h2>
+
+            <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+              <div className="lg:col-span-4 w-full items-center">
+               
+              <section className="text-gray-600 body-font ">
+
+<div className="container px-5 py-3 mx-auto  ">
+  <div className="flex flex-wrap -m-4 justify-center">
+   
+    
+  <Size_Color products={products}/>
+
+
+
+
+    
+  </div>
+</div>
+</section>
+                </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+
+    </div>
+  )
+}
+export async function getServerSideProps(context) {
+  if(!mongoose.connections[0].readystate){
+    await mongoose.connect(process.env.MONGO_URI)
+}
+
+  let products = await Product.find({sub_category:'acp baskets'})
+  let hinges = {}
+  for(let item of products){
+    if(item.title in hinges){
+        if(!hinges[item.title].color.includes(item.color) && item.availableQty > 0){
+          hinges[item.title].color.push(item.color)
+        }
+        if(!hinges[item.title].size.includes(item.size) && item.availableQty > 0){
+          hinges[item.title].size.push(item.size)
+        }
+    }
+
+    else{
+      hinges[item.title] = JSON.parse(JSON.stringify(item))
+      if(item.availableQty >0){
+            hinges[item.title].color = [item.color]
+            hinges[item.title].size = [item.size]
+          }else{
+            hinges[item.title].color = []
+            hinges[item.title].size = []
+          }
+    }
+  }
+
+  return{
+    props:{products:JSON.parse(JSON.stringify(hinges))},
+  }
+  }
+export default Hinges
