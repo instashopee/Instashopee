12a0206e41f46be14236a6d1df1848dc51f8061e
Instashopee <instashopeeonline@gmail.com>
2023-10-24 19:34:34 +0530

    c

--- pages/search.js
+++ pages/search.js
@@ -48,7 +48,7 @@ const search = ({products}) => {
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
          
         </div>
-        <div class=" mb-4">
+        <div class="mx-24 mb-4">
           
              <input
                onChange={handleChanged}
@@ -57,7 +57,7 @@ const search = ({products}) => {
                placeholder='YOU CAN SEARCH HERE ALSO...'
                id="search"
                name="search"
-               class="w-full mt-6 mx-24 bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+               class="w-full mt-6 bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
              />
            </div>        
        {<button  type="submit" onClick={submit} class="text-black absolute right-2.5 bottom-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>}
